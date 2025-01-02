822b694054004585bbb73855e4876ee90711d4d9
Abdullah Abdulrahman Alahiadeb <aalahiadeb.t@barq.com>
2025-01-02 14:21:27 +0300

    deleted redundent folder

--- scheduler/src/main/java/org/example/scheduler/repository/CandidateRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.scheduler.repository;
-
-import org.example.scheduler.model.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
-}
